413b5f9363c14d796c742684cd0e67d8f4e8e451
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-02-17 22:35:33 +0530

    Test GUI using tkinter

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+top = tkinter.Tk()
+frame = tkinter.Frame(background="#34A2FE")
+heading = tkinter.Label(text="This is a test", background="#34A2FE",foreground="white")
+button = tkinter.Button(text="test")
+frame.pack()
+button.pack()
+heading.pack()
+
+top.mainloop()
+tkinter.Button()
