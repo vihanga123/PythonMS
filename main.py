0567ac19ee2c5482cbc89851732c7f72f21cbdb2
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-02-19 21:10:02 +0530

    basic GUI design for a login form

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import tkinter
+from tkinter import *
 
-top = tkinter.Tk()
-frame = tkinter.Frame(background="#34A2FE")
+top=Tk()
+top.title("Student LMS")
+top.geometry("400x200")
 heading = tkinter.Label(text="This is a test", background="#34A2FE",foreground="white")
-button = tkinter.Button(text="test")
-frame.pack()
-button.pack()
-heading.pack()
+heading.grid(row=0,column=2)
+
+label=tkinter.Label(text="Insert Name: ")
+label.grid(row=2,column=1)
+entry=tkinter.Entry()
+entry.grid(row=2,column=2)
+
+label=tkinter.Label(text="Insert Password: ")
+label.grid(row=4,column=1)
+entry=tkinter.Entry()
+entry.grid(row=4,column=2)
+
+button = tkinter.Button(text='test')
+button.grid(row=5,column=1)
+button2 = tkinter.Button(text='test')
+button2.grid(row=5,column=2)
 
 top.mainloop()
-tkinter.Button()
