1da2296741bd938212a6f21c74a5ad5fc54b2006
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-02-27 22:59:54 +0530

    fixed code to retrieve user inputs from the database

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter
 import sqlite3
 from tkinter import *
 
-#table
+# table
 conn = sqlite3.connect('test1.db')
 
 conn.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT,password TEXT)")
@@ -10,58 +10,52 @@ conn.execute("INSERT INTO users (username,password) VALUES('tester','ij')")
 conn.commit()
 print("Success", "User added successfully!")
 
+
 def clear():
-    user.delete(0,tkinter.END)
-    password.delete(0,tkinter.END)
+    user.delete(0, tkinter.END)
+    password.delete(0, tkinter.END)
 
 
-
-top=Tk()
+top = Tk()
 top.title("Student LMS")
-#top.geometry("300x200")
-heading = tkinter.Label(text="This is a test", background="#34A2FE",foreground="white")
-heading.grid(row=0,column=2)
+# top.geometry("300x200")
+heading = tkinter.Label(text="Login Form", background="#34A2FE", foreground="white")
+heading.grid(row=0, column=2)
 
-label=tkinter.Label(text="Insert Name: ")
-label.grid(row=2,column=1)
-user=tkinter.Entry()
-user.grid(row=2,column=2)
+label = tkinter.Label(text="Insert Name: ")
+label.grid(row=2, column=1)
+user = tkinter.Entry()
+user.grid(row=2, column=2)
 
-label=tkinter.Label(text="Insert Password: ")
-label.grid(row=4,column=1)
-password=tkinter.Entry(show="*")
-password.grid(row=4,column=2)
+label = tkinter.Label(text="Insert Password: ")
+label.grid(row=4, column=1)
+password = tkinter.Entry(show="*")
+password.grid(row=4, column=2)
 
+button2 = tkinter.Button(text='clear', command=clear)
+button2.grid(row=5, column=2)
 
-button2 = tkinter.Button(text='clear',command=clear)
-button2.grid(row=5,column=2)
 
 def login():
-
-
     cursor = conn.execute("SELECT * FROM users")
     check = conn.cursor()
-    if check is not None:
-        print("Success")
-    else:
-        print("Invalid")
 
+    uname = user.get()
+    pw = password.get()
 
-    username= user.get()
-    pw=password.get()
-
-    cursor = conn.execute("SELECT username,password FROM users ")
+    cursor = conn.execute("SELECT username,password FROM users WHERE username=? AND password=? ", [uname, pw])
     users = cursor.fetchone()
-    print (users)
+    print(users)
 
-    if users is not None and users[1] == username:
-        print("Login successful")
+    if users is not None and users[1] == uname:
+        if users is not None and users[1] == password:
+            print("Login successful")
     else:
         print("Login failed")
 
 
-button = tkinter.Button(text="Input",command= login)
-button.grid(row=5,column=1)
+button = tkinter.Button(text="Input", command=login)
+button.grid(row=5, column=1)
 
 top.mainloop()
 conn.close()
