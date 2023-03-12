2af8c3449415f6e891b2b0bb703272a9feb8747c
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-03-12 20:33:02 +0530

    Create testfile.py

---
+++ testfile.py
@@ -0,0 +1,19 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QPushButton
+
+"""
+def button_clicked(data):
+    print("You have clicked the button",data)
+
+
+app = QApplication()
+
+button = QPushButton("Press this")
+button.setCheckable(True)
+button.clicked.connect(button_clicked)
+
+button.show()
+app.exec()
+
+"""
