682fdab7edb493c87ddec1178f670ee54c5d4135
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-03-14 21:51:35 +0530

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QSlider, QWidget, QPushButton,QHBoxLayout
+from PySide6.QtWidgets import QApplication, QSlider, QWidget, QPushButton,QVBoxLayout
 
 """
 def button_clicked(data):
@@ -37,8 +37,17 @@ class widget(QWidget):
         super().__init__()
         self.setWindowTitle("Widgets")
         button1 = QPushButton("Button1")
+        button2 = QPushButton("Button 2")
 
-        button_layout = QHBoxLayout()
+        button1.clicked.connect(self.button_message)
+        button2.clicked.connect(self.button_message)
+
+        button_layout = QVBoxLayout()
         button_layout.addWidget(button1)
+        button_layout.addWidget(button2)
+
 
         self.setLayout(button_layout)
+
+    def button_message(self):
+        print("Button Pressed")
