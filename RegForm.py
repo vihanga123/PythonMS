9e2c63707e9d5e2afe0f83b208a6059742684b94
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-03-20 21:46:17 +0530

    database re-implementation and adding data through form

--- RegForm.py
+++ RegForm.py
@@ -1,6 +1,10 @@
 import sys
+import sqlite3
+from sqlite3 import Connection
 from PySide6.QtGui import Qt
-from PySide6.QtWidgets import QVBoxLayout, QLabel, QPushButton, QWidget, QLineEdit, QHBoxLayout
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QPushButton, QWidget, QLineEdit, QHBoxLayout, QComboBox
+
+conn: Connection = sqlite3.connect('Main.db')
 
 
 class register(QWidget):
@@ -11,48 +15,52 @@ class register(QWidget):
         mainText.setAlignment(Qt.AlignCenter)
 
         name = QLabel("Name: ")
-        Nameinput = QLineEdit()
+        self.Nameinput = QLineEdit()
 
         DOB = QLabel("Date of Birth: ")
-        DOBinput = QLineEdit()
-        DOBinput.setInputMask("0000-00-00")
+        self.DOBinput = QLineEdit()
+        self.DOBinput.setInputMask("0000-00-00")
 
         Occupation = QLabel("Occupation: ")
-        Occupationinput = QLineEdit()
+        self.Occupationinput = QComboBox()
+        self.Occupationinput.addItem("Software Engineer")
+        self.Occupationinput.addItem("Clerk")
 
         username = QLabel("Username: ")
-        usernameinput = QLineEdit()
+        self.usernameinput = QLineEdit()
 
         password = QLabel("Password: ")
-        passwordinput = QLineEdit()
-        passwordinput.setEchoMode(QLineEdit.Password)
+        self.passwordinput = QLineEdit()
+        self.passwordinput.setEchoMode(QLineEdit.Password)
 
         Regbutton = QPushButton("Register")
-        clear = QPushButton("Clear")
+        Regbutton.clicked.connect(self.regbutton)
+        clearbtn = QPushButton("Clear")
+        clearbtn.clicked.connect(self.clear)
 
         h_layout1 = QHBoxLayout()
         h_layout1.addWidget(name)
-        h_layout1.addWidget(Nameinput)
+        h_layout1.addWidget(self.Nameinput)
 
         h_layout2 = QHBoxLayout()
         h_layout2.addWidget(DOB)
-        h_layout2.addWidget(DOBinput)
+        h_layout2.addWidget(self.DOBinput)
 
         h_layout3 = QHBoxLayout()
         h_layout3.addWidget(Occupation)
-        h_layout3.addWidget(Occupationinput)
+        h_layout3.addWidget(self.Occupationinput)
 
         h_layout4 = QHBoxLayout()
         h_layout4.addWidget(username)
-        h_layout4.addWidget(usernameinput)
+        h_layout4.addWidget(self.usernameinput)
 
         h_layout5 = QHBoxLayout()
         h_layout5.addWidget(password)
-        h_layout5.addWidget(passwordinput)
+        h_layout5.addWidget(self.passwordinput)
 
         h_layout6 = QHBoxLayout()
         h_layout6.addWidget(Regbutton)
-        h_layout6.addWidget(clear)
+        h_layout6.addWidget(clearbtn)
 
         v_layout = QVBoxLayout()
         v_layout.addWidget(mainText)
@@ -64,3 +72,17 @@ class register(QWidget):
         v_layout.addLayout(h_layout6)
 
         self.setLayout(v_layout)
+
+    def clear(self):
+        self.Nameinput.clear()
+        self.DOBinput.clear()
+        self.usernameinput.clear()
+        self.passwordinput.clear()
+
+    def regbutton(self):
+        conn.execute("INSERT INTO Registration (name,DOB,occupation,username,password) VALUES (?, ?, ?, ?, ?)",
+                     [self.usernameinput.text(), self.DOBinput.text(), self.Occupationinput.currentText(), self.usernameinput.text(), self.passwordinput.text()])
+        print("Student Registered Successfully")
+
+
+
--- main.py
+++ main.py
@@ -165,8 +165,18 @@ conn.close()
 """
 
 import sys
+import sqlite3
+from sqlite3 import Connection
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QLabel, QMainWindow, QPushButton, QWidget
 from RegForm import register
+
+conn: Connection = sqlite3.connect('Main.db')
+
+conn.execute("CREATE TABLE IF NOT EXISTS Registration (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,DOB DATE,occupation TEXT,username TEXT,password TEXT)")
+# conn.execute("INSERT INTO users (username,password) VALUES('vihanga','123')")
+conn.commit()
+print("Database is running!")
+
 
 app = QApplication(sys.argv)
 window = register()
